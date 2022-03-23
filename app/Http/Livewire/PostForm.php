a10815f59f6be1a815632b884e4713740c23c401
Asyraf Rahim <asyrafboyz@gmail.com>
2022-03-23 11:28:16 +0800

    add store, edit, update, destroy () in PostForm.php

--- app/Http/Livewire/PostForm.php
+++ app/Http/Livewire/PostForm.php
@@ -2,12 +2,64 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class PostForm extends Component
 {
+    use WithPagination;
+
+    public $title;
+    public $body;
+    public $post_id;
+
+    protected $rules = [
+        'title' => 'required',
+        'body' => 'required|min:10',
+    ];
+
+    public function storePost()
+    {
+        $this->validate();
+
+        $post = Post::create([
+            'title' => $this->title,
+            'body' => $this->body,
+            'user_id' => auth()->id(),
+        ]);
+        $this->reset();
+    }
+    
+    public function edit($post)
+    {
+        $post = Post::find($post);
+        $this->post_id = $post->id;
+        $this->title = $post->title;
+        $this->body = $post->body;
+    }
+
+    public function update()
+    {
+        $post = Post::updateOrCreate(
+            [
+                'id' => $this->post_id
+            ],
+            [
+                'title' => $this->title,
+                'body' => $this->body,
+            ]
+        );
+        $this->reset();
+    }
+
+    public function destroy($post)
+    {
+        Post::destroy($post);
+    }
+
     public function render()
     {
-        return view('livewire.post-form');
+        return view('livewire.post-form', ['posts' => Post::latest()->paginate(10)]);
     }
 }
--- database/migrations/2022_03_22_035804_create_posts_table.php
+++ database/migrations/2022_03_22_035804_create_posts_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug');
             $table->text('body');
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
